fc124ee094fbd0b2a8345ad2188e5bb147b18caa
L§Dev <78922125+LSS-commits@users.noreply.github.com>
2021-12-15 15:18:21 +0100

    Update Ko20ProductController.php

--- src/Controller/Ko20ProductController.php
+++ src/Controller/Ko20ProductController.php
@@ -14,11 +14,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
 
+    // CONSTRUCTEUR
     protected $categoryRepository;
     protected $productRepository;
     // protected $urlGenerator;
 
-    // Pour passer les produits et caégories dans les URL
+    // Pour passer les produits et catégories dans les URL
     public function __construct(
         CategoryRepository $categoryRepository,
         ProductRepository $productRepository
@@ -27,61 +28,78 @@ class ProductController extends AbstractController
         $this->productRepository = $productRepository;
     }
 
-    /**
-     * @Route("/{slug}", name="product_category")
-     */
-    public function category($slug): Response
-    {
 
-        $category = $this->categoryRepository->findOneBy([
-            'slug' => $slug
+    // FONCTION POUR AFFICHER PAGE PRODUITS SANS CATEGORIE 
+    private function no_category($category)
+    {
+        // Si on écrit "aucune-categorie" dans l'url, créer artificiellement une catégorie correspondante
+        $category = new Category;
+        $category
+            ->setName("Aucune catégorie")
+            ->setSlug("aucune-categorie");
+
+        // Pour afficher les produits existants qui n'ont pas de catégorie (NULL en bdd) : on va chercher tous les produits ayant une catégorie NULL en bdd et on les passe dans $category
+        $products = $this->productRepository->findBy([
+            'category' => NULL,
         ]);
 
-        // Si on écrit "aucune-categorie" dans l'url, créer artificiellement une catégorie correspondante
-        if ($slug === "aucune-categorie") {
-            $category = new Category;
-            $category
-                ->setName("Aucune catégorie")
-                ->setSlug("aucune-categorie");
+        foreach ($products as $product) {
+            $category->addProduct($product);
+        }
+        return $category;
+    }
 
-            // Pour afficher les produits existants qui n'ont pas de catégorie (NULL en bdd) : on va chercher tous les produits ayant une catégorie NULL en bdd et on les passe dans $category
-            $products = $this->productRepository->findBy([
-                'category' => NULL,
-            ]);
+    // PAGE CATEGORIES
+    /**
+     * @Route("/{category_slug}", name="product_category")
+     */
+    public function category($category_slug): Response
+    {
+        // Récupérer la catégorie existante en bdd
+        $category = $this->categoryRepository->findOneBy([
+            'slug' => $category_slug
+        ]);
 
-            foreach ($products as $product) {
-                $category->addProduct($product);
+        // Si la catégorie recherchée n'existe pas en bdd = soit produits sans catégorie -> page aucune catégorie, soit erreur 404
+        if (!$category) {
+            if ($category_slug === "aucune-categorie") {
+                $category = $this->no_category($category);
+            } else {
+                throw $this->createNotFoundException("La catégorie recherchée n'existe pas");
             }
         }
 
-        // Si on passe une catégorie inexistante dans l'url
-        if (!$category) {
-            throw $this->createNotFoundException("La catégorie recherchée n'existe pas");
-        }
-
         return $this->render('product/category.html.twig', [
-            'category' => $category,
+            'category' => $category
         ]);
     }
 
+
+    // PAGE PRODUIT
     /**
      * @Route("/{category_slug}/{product_id}-{product_slug}", name="product_show")
      */
     public function show($category_slug, $product_id, $product_slug): Response
     {
+        // Récupérer la catégorie existante en bdd
         $category = $this->categoryRepository->findOneBy([
             'slug' => $category_slug
         ]);
 
+        // Récupérer les produits correspondants
         $product = $this->productRepository->findOneBy([
             'id' => $product_id,
             'slug' => $product_slug,
             'category' => $category
         ]);
 
-        // Si la catégorie recherchée n'existe pas en bdd / n'est pas "aucune-categorie"
-        if (!$category && $category_slug !== "aucune-categorie") {
-            throw $this->createNotFoundException("Le produit recherché n'existe pas");
+        // Si la catégorie recherchée n'existe pas en bdd = soit produits sans catégorie -> page aucune catégorie, soit erreur 404
+        if (!$category) {
+            if ($category_slug === "aucune-categorie") {
+                $category = $this->no_category($category);
+            } else {
+                throw $this->createNotFoundException("La catégorie et/ou le produit recherché-e-s n'existe-nt pas.");
+            }
         }
 
         // Si le produit n'existe pas 
@@ -91,6 +109,7 @@ class ProductController extends AbstractController
 
         return $this->render('product/show.html.twig', [
             'product' => $product,
+            'category' => $category
         ]);
     }
 }
